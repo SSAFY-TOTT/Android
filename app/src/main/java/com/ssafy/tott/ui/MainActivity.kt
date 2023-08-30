263276c85f235fa2018a40b7fa5ced3161f05327
ChoiUS <chldn323@gmail.com>
2023-08-30 23:45:37 +0900

    feat: 기본 지도 화면 추가

--- app/src/main/java/com/ssafy/tott/ui/MainActivity.kt
+++ app/src/main/java/com/ssafy/tott/ui/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.ssafy.tott.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ssafy.tott.R
+import com.ssafy.tott.ui.map.SearchMapActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,5 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = Intent(this, SearchMapActivity::class.java)
+        startActivity(intent)
     }
 }
