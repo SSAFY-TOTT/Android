4d2483ff3a2f98557b45a69f93130b41a7df80e4
ChoiUS <chldn323@gmail.com>
2023-08-30 23:44:21 +0900

    feat: app 모듈에 Hilt 적용

--- app/src/main/java/com/ssafy/tott/ui/MainActivity.kt
+++ app/src/main/java/com/ssafy/tott/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.ssafy.tott
+package com.ssafy.tott.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ssafy.tott.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
