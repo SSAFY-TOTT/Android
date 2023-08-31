ffc660a4cbb75048dd5455db8239424d56e65b3c
ChoiUS <chldn323@gmail.com>
2023-08-31 21:50:58 +0900

    feat: 지도에 클러스터 적용

--- app/src/main/java/com/ssafy/tott/ui/map/SearchMapActivity.kt
+++ app/src/main/java/com/ssafy/tott/ui/map/SearchMapActivity.kt
@@ -1,21 +1,26 @@
 package com.ssafy.tott.ui.map
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Marker
+import com.google.maps.android.clustering.ClusterManager
 import com.ssafy.tott.R
 import com.ssafy.tott.databinding.ActivitySearchMapBinding
+import com.ssafy.tott.ui.model.ClusterMarker
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SearchMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var map: GoogleMap
     private lateinit var binding: ActivitySearchMapBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,27 +28,35 @@ class SearchMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
         binding = ActivitySearchMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.fragment_search_map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        map = googleMap
+        lifecycleScope.launch {
+            val lat = 38.0
+            val lng = 127.0
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(lat, lng), 5F))
+            map.clear()
+            setUpCluster()
+        }
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private fun setUpCluster() {
+        val clusterManager = ClusterManager<ClusterMarker>(this, map)
+
+        clusterManager.markerCollection.setInfoWindowAdapter(object : GoogleMap.InfoWindowAdapter {
+            override fun getInfoContents(p0: Marker): View {
+                return View(applicationContext)
+            }
+
+            override fun getInfoWindow(p0: Marker): View {
+                return View(applicationContext)
+            }
+        })
+        map.setOnCameraIdleListener(clusterManager)
+//        setupMapClickListener(clusterManager)
     }
 }
