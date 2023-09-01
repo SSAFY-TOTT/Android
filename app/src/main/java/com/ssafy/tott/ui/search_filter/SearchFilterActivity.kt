378a2c7353f08b0110932bb726664194c5f7868c
ChoiUS <chldn323@gmail.com>
2023-09-01 23:48:01 +0900

    feat(#1): 검색 필터 화면 및 툴바 추가

---
+++ app/src/main/java/com/ssafy/tott/ui/search_filter/SearchFilterActivity.kt
@@ -0,0 +1,12 @@
+package com.ssafy.tott.ui.search_filter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssafy.tott.R
+
+class SearchFilterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_filter)
+    }
+}
