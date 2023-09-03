d3c6164bc5da1594bacc37d8ad85b19d84c554b5
ChoiUS <chldn323@gmail.com>
2023-09-03 17:52:13 +0900

    feat(#9): 매물 목록 아이템 구현
    - 사진, 주소, 가격, 면적

--- app/src/main/java/com/ssafy/tott/ui/houselist/BuildingDetailViewHolder.kt
+++ app/src/main/java/com/ssafy/tott/ui/houselist/BuildingDetailViewHolder.kt
@@ -1,12 +1,20 @@
 package com.ssafy.tott.ui.houselist
 
 import androidx.recyclerview.widget.RecyclerView
+import com.ssafy.tott.R
 import com.ssafy.tott.databinding.ItemBuildingDetailBinding
 import com.ssafy.tott.ui.model.BuildingDetailUI
 
 class BuildingDetailViewHolder(private val binding: ItemBuildingDetailBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(item: BuildingDetailUI) {
-        binding.tvSimpleHouseItem.text = item.toString()
+        binding.root.context.run {
+            binding.tvAreaItemBuildingDetail.text =
+                getString(R.string.area_buildingDetail_item, item.area)
+            binding.tvAddressItemBuildingDetail.text =
+                getString(R.string.address_buildingDetail_item, item.simpleAddress)
+            binding.tvPriceItemBuildingDetail.text =
+                getString(R.string.price_buildingDetail_item, item.price)
+        }
     }
 }
