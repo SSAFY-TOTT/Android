d0d6b6f99ada75dab47e7ead3fde21a98763d6ce
ChoiUS <chldn323@gmail.com>
2023-09-03 14:12:46 +0900

    feat(#5): 매물 데이터 변경
    - 좌표 제거
    - 아이디, 가격, 면적, 층수 추가

--- app/src/main/java/com/ssafy/tott/domain/model/House.kt
+++ app/src/main/java/com/ssafy/tott/domain/model/House.kt
@@ -1,6 +1,8 @@
 package com.ssafy.tott.domain.model
 
 data class House(
-    val lat: Double,
-    val lng: Double,
+    val id: Int,
+    val price: Int, // 가격 단위: 백만원
+    val area: Int, // 면적 단위: 평
+    val floor: Int?,
 )
