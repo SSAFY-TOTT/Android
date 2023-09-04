99355b13f0b0d5920ec4701658665ef4f0bce389
ChoiUS <chldn323@gmail.com>
2023-09-05 00:12:44 +0900

    feat(#13): 필터 설정 화면 툴바 초기 설정

--- app/src/main/java/com/ssafy/tott/ui/searchfilter/SearchFilterActivity.kt
+++ app/src/main/java/com/ssafy/tott/ui/searchfilter/SearchFilterActivity.kt
@@ -1,9 +1,13 @@
 package com.ssafy.tott.ui.searchfilter
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import com.google.android.material.textfield.TextInputLayout
+import com.ssafy.tott.R
 import com.ssafy.tott.databinding.ActivitySearchFilterBinding
 
 class SearchFilterActivity : AppCompatActivity() {
@@ -14,6 +18,35 @@ class SearchFilterActivity : AppCompatActivity() {
         binding = ActivitySearchFilterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initDropDown()
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(binding.toolbarSearchFilter)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar_search_filter, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        android.R.id.home -> {
+            Log.d(this::class.simpleName, "onOptionsItemSelected: home")
+            true
+        }
+
+        R.id.action_saveFilter -> {
+            Log.d(this::class.simpleName, "onOptionsItemSelected: save")
+            true
+        }
+
+        else -> {
+            Log.d(
+                this::class.simpleName, "onOptionsItemSelected: else -> ${item.itemId}"
+            )
+            super.onOptionsItemSelected(item)
+        }
     }
 
     private fun initDropDown() {
@@ -35,5 +68,7 @@ class SearchFilterActivity : AppCompatActivity() {
         private val addressArray1 = arrayOf("강남구", "서초구", "중구")
         private val addressArray2 = arrayOf("역삼1동", "논현1동", "신사동")
         private val builtArray = arrayOf("전체", "5년전", "10년전", "15년전")
+
+        const val TAG_DISTRICT_NAME = "TAG_DISTRICT_NAME"
     }
 }
