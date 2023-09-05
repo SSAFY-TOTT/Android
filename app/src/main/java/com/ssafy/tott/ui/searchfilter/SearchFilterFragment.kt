3ee2fe766a22df3a4b8fd7876718f73e532bb229
ChoiUS <chldn323@gmail.com>
2023-09-05 23:11:08 +0900

    feat(#13): 집 찾기 필터 화면 프래그먼트 버전 추가

---
+++ app/src/main/java/com/ssafy/tott/ui/searchfilter/SearchFilterFragment.kt
@@ -0,0 +1,83 @@
+package com.ssafy.tott.ui.searchfilter
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.ssafy.tott.R
+import com.ssafy.tott.databinding.FragmentSearchFilterBinding
+import com.ssafy.tott.ui.map.SearchMapActivity
+
+class SearchFilterFragment : Fragment() {
+    private val searchFilterViewModel: SearchMapViewModel by activityViewModels()
+    private var _binding: FragmentSearchFilterBinding? = null
+    private val binding get() = _binding!!
+    private val callback: OnBackPressedCallback by lazyOf(initOnBackPressedCallback())
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchFilterBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+
+    private fun initOnBackPressedCallback(): OnBackPressedCallback {
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                (requireActivity() as SearchMapActivity).menuInflate(R.menu.menu_search_map)
+                val fragmentManager = requireActivity().supportFragmentManager
+                fragmentManager.beginTransaction().remove(this@SearchFilterFragment).commit()
+                Log.d("SearchFilterFragment", "handleOnBackPressed: remove Fragment")
+                fragmentManager.popBackStack()
+            }
+        }
+        return callback
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initToolbar()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    private fun initToolbar() {
+        binding.toolbarSearchFilter.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                android.R.id.home -> {
+                    Log.d(this::class.simpleName, "onOptionsItemSelected: home")
+                    true
+                }
+
+                R.id.action_saveFilter -> {
+                    Log.d(this::class.simpleName, "onOptionsItemSelected: save")
+                    true
+                }
+
+                else -> {
+                    Log.d(
+                        this::class.simpleName, "onOptionsItemSelected: else -> ${item.itemId}"
+                    )
+                    false
+                }
+            }
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
