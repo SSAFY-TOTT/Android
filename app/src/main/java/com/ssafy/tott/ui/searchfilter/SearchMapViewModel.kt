3706dd893f4f305fe33ff2adea3c31ab395787b6
ChoiUS <chldn323@gmail.com>
2023-09-05 23:09:39 +0900

    feat(#13): SearchMapActivity에 사용할 ViewModel 추가
    - 내용 미구현

---
+++ app/src/main/java/com/ssafy/tott/ui/searchfilter/SearchMapViewModel.kt
@@ -0,0 +1,9 @@
+package com.ssafy.tott.ui.searchfilter
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@ViewModelScoped
+class SearchMapViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
