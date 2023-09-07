7dfa861b276ef1fe91760e80cc21e8501206a104
ChoiUS <chldn323@gmail.com>
2023-09-07 21:23:20 +0900

    feat(#14): 집 찾기 Repository, DataSource를 Hilt로 의존성 적용

--- app/src/main/java/com/ssafy/tott/di/DataModule.kt
+++ app/src/main/java/com/ssafy/tott/di/DataModule.kt
@@ -1,5 +1,9 @@
 package com.ssafy.tott.di
 
+import com.ssafy.tott.data.datasource.BuildingDataSource
+import com.ssafy.tott.data.datasource.remote.BuildingRemoteDataSource
+import com.ssafy.tott.data.repository.BuildingRepositoryImpl
+import com.ssafy.tott.domain.repository.SearchBuildingRepository
 import com.ssafy.tott.data.datasource.AccountDataSource
 import com.ssafy.tott.data.datasource.remote.AccountDataSourceRemote
 import dagger.Module
@@ -11,6 +15,16 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object DataModule {
+    @Provides
+    @Singleton
+    fun provideSearchBuildingRepository(buildingDataSource: BuildingDataSource): SearchBuildingRepository =
+        BuildingRepositoryImpl(buildingDataSource)
+
+    @Provides
+    @Singleton
+    fun provideSearchBuildingDataSource(): BuildingDataSource =
+        BuildingRemoteDataSource()
+
     @Provides
     @Singleton
     fun provideAccountDataSource(): AccountDataSource =
