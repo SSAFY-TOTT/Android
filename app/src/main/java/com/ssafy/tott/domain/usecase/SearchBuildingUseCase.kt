0fdbe690ff7bf9364fc69eba5590e0ec07b1c91b
ChoiUS <chldn323@gmail.com>
2023-09-07 21:23:37 +0900

    feat(#14): 집 찾기 UseCase 구현

---
+++ app/src/main/java/com/ssafy/tott/domain/usecase/SearchBuildingUseCase.kt
@@ -0,0 +1,13 @@
+package com.ssafy.tott.domain.usecase
+
+import com.ssafy.tott.domain.model.Building
+import com.ssafy.tott.domain.model.SearchFilter
+import com.ssafy.tott.domain.repository.SearchBuildingRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SearchBuildingUseCase @Inject constructor(private val buildingRepository: SearchBuildingRepository) {
+    operator fun invoke(searchFilter: SearchFilter): Flow<List<Building>> {
+        return buildingRepository.getBuildings(searchFilter)
+    }
+}
