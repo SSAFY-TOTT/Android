45cbc1cf8ac240cdae4df01f759933fc6fa44645
ChoiUS <chldn323@gmail.com>
2023-09-07 22:47:07 +0900

    chore(#14): 매물 임시 데이터 수정

--- app/src/main/java/com/ssafy/tott/data/datasource/remote/BuildingRemoteDataSource.kt
+++ app/src/main/java/com/ssafy/tott/data/datasource/remote/BuildingRemoteDataSource.kt
@@ -11,7 +11,7 @@ class BuildingRemoteDataSource : BuildingDataSource {
     override fun searchBuilding(searchFilter: SearchFilter): Flow<List<Building>> = flow {
         // TODO: 임시 데이터
         val list = listOf<Building>(
-            Building(127.0498016, 37.5052464, listOf(), "동", "구", "건물"),
+            Building(37.5152464, 127.0428016, listOf(BuildingDetail(234, 122, 18, 2)), "동", "구", "건물"),
             Building(37.5052464, 127.0498016, listOf(BuildingDetail(0, 1, 3, null)), "동", "구", "건물")
         )
         emit(list)
