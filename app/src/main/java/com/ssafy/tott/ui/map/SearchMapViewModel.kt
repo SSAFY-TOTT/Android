c1d2ceb88b11353a1427cb52c2f331b89894f043
ChoiUS <chldn323@gmail.com>
2023-09-07 21:24:57 +0900

    feat(#14): SearchMapViewModel에 매물 데이터 가져오는 기능 구현

--- app/src/main/java/com/ssafy/tott/ui/map/SearchMapViewModel.kt
+++ app/src/main/java/com/ssafy/tott/ui/map/SearchMapViewModel.kt
@@ -3,11 +3,22 @@ package com.ssafy.tott.ui.map
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ssafy.tott.domain.model.Building
+import com.ssafy.tott.domain.model.SearchFilter
+import com.ssafy.tott.domain.usecase.SearchBuildingUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SearchMapViewModel @Inject() constructor() : ViewModel() {
+class SearchMapViewModel @Inject constructor(val useCase: SearchBuildingUseCase) :
+    ViewModel() {
+    private val _buildings = MutableStateFlow<List<Building>>(listOf())
+    val buildings = _buildings.asStateFlow()
+
     private val districtName = MutableLiveData<String>("강남구")
     private val legalDongName = MutableLiveData<String>("역삼1동")
     private val minPrice = MutableLiveData<Int>(13)
@@ -54,6 +65,24 @@ class SearchMapViewModel @Inject() constructor() : ViewModel() {
     }
 
     private fun loadFilteredData() {
-        // TODO 필터 설정 데이터 전송 필요
+        val districtName = districtName.value ?: return
+        val legalDongName = legalDongName.value ?: return
+        val built = 0
+        val minArea = minArea.value ?: return
+        val maxArea = maxArea.value ?: return
+        val minPrice = minPrice.value ?: return
+        val maxPrice = maxPrice.value ?: return
+        val type = type.value ?: return
+        val searchFilter = SearchFilter(
+            districtName, legalDongName, minPrice,
+            maxPrice, minArea, maxArea,
+            type, built
+        )
+        viewModelScope.launch {
+            useCase(searchFilter).collect {
+                Log.d(this::class.simpleName, "loadFilteredData: $it")
+                _buildings.value = it
+            }
+        }
     }
 }
