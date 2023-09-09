d60cc4eff4b83d98eab602801e57f9e2df79f9fe
ChoiUS <chldn323@gmail.com>
2023-09-09 14:27:53 +0900

    feat(#21): UiState를 각 ViewModel에서 관리하도록 변경

--- app/src/main/java/com/ssafy/tott/ui/UiState.kt
+++
@@ -1,8 +0,0 @@
-package com.ssafy.tott.ui
-
-
-sealed class UiState {
-    data object Loading : UiState()
-    data class Error<E : Throwable>(val data: E) : UiState()
-    data class Success<R>(val data: R) : UiState()
-}
