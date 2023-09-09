6589338e9fea0df1770cc63229e3fb5ad0e41398
ChoiUS <chldn323@gmail.com>
2023-09-09 14:28:42 +0900

    feat(#21): 회원 가입 시 인증 번호 요청 후 UI 반영

--- app/src/main/java/com/ssafy/tott/domain/model/RegisterUser.kt
+++ app/src/main/java/com/ssafy/tott/domain/model/RegisterUser.kt
@@ -6,4 +6,10 @@ data class RegisterUser(
     val password2: String,
     val phone: String,
     val accountNum: String
-)
+) {
+    fun isValid() = blankCheck(id, password, password2, phone, accountNum).not()
+
+    private fun blankCheck(vararg str: String): Boolean {
+        return str.all { it.isNotBlank() }
+    }
+}
--- app/src/main/java/com/ssafy/tott/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/ssafy/tott/ui/register/RegisterViewModel.kt
@@ -1,26 +1,40 @@
 package com.ssafy.tott.ui.register
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ssafy.tott.domain.model.RegisterUser
 import com.ssafy.tott.domain.usecase.RegisterUserUseCase
-import com.ssafy.tott.ui.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RegisterViewModel @Inject constructor(private val registerUseCase: RegisterUserUseCase) :
     ViewModel() {
 
-    private val _uiState = MutableStateFlow<UiState>(UiState.Success(Unit))
-    val uiState = _uiState.asStateFlow()
+    sealed class UiState {
+        data object Loading : UiState()
+        data class Error(val throwable: Throwable) : UiState()
+        data object Success : UiState()
+    }
+
+    private val _uiState = Channel<UiState>()
+    val uiState = _uiState.receiveAsFlow()
+
     fun makeCertNum(registerUser: RegisterUser) {
-        _uiState.value = UiState.Loading
-        registerUseCase.invoke(registerUser).onSuccess {
-            _uiState.value = UiState.Success(Unit)
-        }.onFailure {
-            _uiState.value = UiState.Error(it)
+        viewModelScope.launch {
+            if (registerUser.isValid()) {
+                _uiState.send(UiState.Error(Throwable("다시 입력해 주세요.")))
+                return@launch
+            }
+            _uiState.send(UiState.Loading)
+            registerUseCase.invoke(registerUser).onSuccess {
+                _uiState.send(UiState.Success)
+            }.onFailure {
+                _uiState.send(UiState.Error(it))
+            }
         }
     }
 }
