ad2513270d70e422fa0ac8de23d28bd11681be78
ChoiUS <chldn323@gmail.com>
2023-09-13 16:13:27 +0900

    feat(#36): 계정 관리 Repository 기능 추가

--- app/src/main/java/com/ssafy/tott/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/ssafy/tott/data/repository/UserRepositoryImpl.kt
@@ -1,14 +1,36 @@
 package com.ssafy.tott.data.repository
 
-import com.ssafy.tott.data.datasource.AccountDataSource
+import com.ssafy.tott.data.datasource.UserDataSource
 import com.ssafy.tott.data.datasource.mapper.toData
+import com.ssafy.tott.data.model.response.AuthTokenRemoteResponse
+import com.ssafy.tott.domain.model.AuthToken
 import com.ssafy.tott.domain.model.RegisterUser
 import com.ssafy.tott.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
-class UserRepositoryImpl @Inject constructor(private val accountDataSource: AccountDataSource) :
+class UserRepositoryImpl @Inject constructor(private val userDataSource: UserDataSource) :
     UserRepository {
     override fun makeCertNum(registerUser: RegisterUser): Result<Unit> {
-        return accountDataSource.makeCertNum(registerUser.toData())
+        return userDataSource.makeCertNum(registerUser.toData())
+    }
+
+    override fun login(id: String, password: String): Flow<Result<AuthToken>> {
+        return userDataSource.login(id, password).map { result ->
+            result.map(AuthTokenRemoteResponse::toDomain)
+        }
+    }
+
+    override fun refreshToken(token: String): Flow<Result<AuthToken>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun saveToken(authToken: AuthToken): Flow<Result<Unit>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getToken(): Flow<Result<String>> {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/ssafy/tott/domain/repository/UserRepository.kt
+++ app/src/main/java/com/ssafy/tott/domain/repository/UserRepository.kt
@@ -1,7 +1,13 @@
 package com.ssafy.tott.domain.repository
 
+import com.ssafy.tott.domain.model.AuthToken
 import com.ssafy.tott.domain.model.RegisterUser
+import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
     fun makeCertNum(registerUser: RegisterUser): Result<Unit>
+    fun login(id: String, password: String): Flow<Result<AuthToken>>
+    fun refreshToken(token: String): Flow<Result<AuthToken>>
+    fun saveToken(authToken: AuthToken): Flow<Result<Unit>>
+    fun getToken(): Flow<Result<String>>
 }
