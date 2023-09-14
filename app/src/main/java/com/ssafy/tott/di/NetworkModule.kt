713b976650e27e3093f11e88e216e4dc25d5f441
ChoiUS <chldn323@gmail.com>
2023-09-14 23:31:04 +0900

    feat(#42): 토큰 갱신을 요청하는 기능 구현

--- app/src/main/java/com/ssafy/tott/di/NetworkModule.kt
+++ app/src/main/java/com/ssafy/tott/di/NetworkModule.kt
@@ -1,13 +1,20 @@
 package com.ssafy.tott.di
 
+import android.util.Log
 import com.google.maps.android.v3.ktx.BuildConfig
+import com.ssafy.tott.data.datasource.local.DataStoreManager
 import com.ssafy.tott.data.datasource.remote.service.UserService
+import com.ssafy.tott.data.model.response.AuthTokenRemoteResponse
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import okhttp3.Authenticator
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -22,15 +29,18 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideOkHttpClient() = if (BuildConfig.DEBUG) {
+    fun provideOkHttpClient(
+        authAuthenticator: Authenticator,
+    ) = if (BuildConfig.DEBUG) {
         OkHttpClient.Builder()
+            .authenticator(authAuthenticator)
             .addInterceptor(
                 HttpLoggingInterceptor()
                     .setLevel(HttpLoggingInterceptor.Level.BODY)
             )
             .build()
     } else {
-        OkHttpClient.Builder().build()
+        OkHttpClient.Builder().authenticator(authAuthenticator).build()
     }
 
     @Singleton
@@ -53,5 +63,42 @@ object NetworkModule {
     @Singleton
     fun provideUserApiService(retrofit: Retrofit): UserService {
         return retrofit.create(UserService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideTokenAuthenticator(
+        dataStoreManager: DataStoreManager,
+    ) = Authenticator { _, response ->
+        runBlocking {
+            val newTokenResponse = getNewToken(dataStoreManager.refreshToken.first())
+
+            if (!newTokenResponse.isSuccessful || newTokenResponse.body() == null) { //Couldn't refresh the token, so restart the login process
+                // TODO Refresh가 불가능하면 Home 화면으로 이동
+                Log.d("NetworkModule", "provideTokenAuthenticator: refresh fail")
+            }
+
+            newTokenResponse.body()?.let {
+                dataStoreManager.setAccessToken(it.accessToken)
+                dataStoreManager.setRefreshToken(it.refreshToken)
+                response.request.newBuilder()
+                    .header("Authorization", it.accessToken)
+                    .build()
+            }
+        }
+    }
+
+    private suspend fun getNewToken(refreshToken: String?): Response<AuthTokenRemoteResponse> {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        val okHttpClient = OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+        val service = retrofit.create(UserService::class.java)
+        return service.refreshToken(refreshToken ?: "")
     }
 }
