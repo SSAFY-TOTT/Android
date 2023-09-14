ccae6d293a9ae3e5b4fac1e9433ccb50db313ad3
ChoiUS <chldn323@gmail.com>
2023-09-14 15:53:28 +0900

    refactor(#38): 사용하지 않는 파일 제거

--- app/src/main/java/com/ssafy/tott/data/datasource/mapper/UserMapper.kt
+++
@@ -1,7 +0,0 @@
-package com.ssafy.tott.data.datasource.mapper
-
-import com.ssafy.tott.data.model.RegisterAccountRequest
-import com.ssafy.tott.domain.model.RegisterUser
-
-fun RegisterUser.toData() =
-    RegisterAccountRequest(id, password, validPassword, phone, accountNum)
--- app/src/main/java/com/ssafy/tott/data/model/RegisterAccountRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.ssafy.tott.data.model
-
-data class RegisterAccountRequest(
-    val id: String,
-    val password: String,
-    val validPassword: String,
-    val phone: String,
-    val accountNum: String,
-)
