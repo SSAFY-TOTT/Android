8e55d8e1b1d70d2ad0b6217152d461a5fd8780ad
ChoiUS <chldn323@gmail.com>
2023-09-14 22:22:48 +0900

    feat(#42): 만료된 토큰을 알리는 Exception 구현

--- app/src/main/java/com/ssafy/tott/domain/exception/Exceptions.kt
+++ app/src/main/java/com/ssafy/tott/domain/exception/Exceptions.kt
@@ -5,3 +5,9 @@ class NetworkException(
     override val message: String? = null,
     override val cause: Throwable? = null
 ) : RuntimeException(message, cause)
+
+class ExpiredTokenException(
+    val code: Int?,
+    override val message: String? = null,
+    override val cause: Throwable? = null
+) : RuntimeException(message, cause)
