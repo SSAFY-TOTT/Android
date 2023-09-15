6b32838af185ceba3f275050fefc34aae174b2f9
ChoiUS <chldn323@gmail.com>
2023-09-15 14:26:34 +0900

    feat(#44): 최근 본 매물 요청 DataSource 구현

--- app/src/main/java/com/ssafy/tott/data/datasource/BuildingDataSource.kt
+++ app/src/main/java/com/ssafy/tott/data/datasource/BuildingDataSource.kt
@@ -1,9 +1,11 @@
 package com.ssafy.tott.data.datasource
 
 import com.ssafy.tott.data.model.request.SearchFilterRequest
+import com.ssafy.tott.data.model.response.BuildingDetailsResponse
 import com.ssafy.tott.data.model.response.BuildingListResponse
 import kotlinx.coroutines.flow.Flow
 
 interface BuildingDataSource {
     fun searchBuilding(searchFilter: SearchFilterRequest): Flow<Result<BuildingListResponse>>
+    fun getRecentHouseArticles(ids: List<Int>): Flow<Result<BuildingDetailsResponse?>>
 }
--- app/src/main/java/com/ssafy/tott/data/datasource/remote/BuildingRemoteDataSource.kt
+++ app/src/main/java/com/ssafy/tott/data/datasource/remote/BuildingRemoteDataSource.kt
@@ -3,7 +3,10 @@ package com.ssafy.tott.data.datasource.remote
 import android.util.Log
 import com.ssafy.tott.data.datasource.BuildingDataSource
 import com.ssafy.tott.data.datasource.remote.service.BuildingService
+import com.ssafy.tott.data.mapper.getErrorResponse
+import com.ssafy.tott.data.model.request.BuildingDetailIdListRequest
 import com.ssafy.tott.data.model.request.SearchFilterRequest
+import com.ssafy.tott.data.model.response.BuildingDetailsResponse
 import com.ssafy.tott.data.model.response.BuildingListResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -35,4 +38,15 @@ class BuildingRemoteDataSource @Inject constructor(private val buildingService: 
                 emit(Result.failure(Throwable("연결에 실패했습니다.")))
             }
         }.flowOn(Dispatchers.IO)
+
+    override fun getRecentHouseArticles(ids: List<Int>) = flow<Result<BuildingDetailsResponse?>> {
+        val response = buildingService.fetchGetRecentBuildings(BuildingDetailIdListRequest(ids))
+        if (response.isSuccessful) {
+            emit(Result.success(response.body()))
+        } else {
+            val errorResponse = getErrorResponse(response.errorBody()?.string() ?: "")
+            Log.d("BuildingDataSourceRemote", "login: $errorResponse}")
+            emit(Result.failure(errorResponse.toNetworkException()))
+        }
+    }
 }
