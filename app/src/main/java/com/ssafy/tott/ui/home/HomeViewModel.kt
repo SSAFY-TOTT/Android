6a7a57e9be15a7198c1b66b60596f4464bcc651e
ChoiUS <chldn323@gmail.com>
2023-09-16 01:39:21 +0900

    feat(#44): WishList 로딩 후 결과에 따라 UI 반영

--- app/src/main/java/com/ssafy/tott/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/ssafy/tott/ui/home/HomeViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ssafy.tott.domain.model.HouseSaleArticle
 import com.ssafy.tott.domain.usecase.GetRecentHouseArticlesUseCase
+import com.ssafy.tott.domain.usecase.GetWishListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,24 +13,34 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val getRecentHouseArticlesUseCase: GetRecentHouseArticlesUseCase
+    private val getRecentHouseArticlesUseCase: GetRecentHouseArticlesUseCase,
+    private val getWishListUseCase: GetWishListUseCase,
 ) : ViewModel() {
     private val _recentBuildingDetails = MutableStateFlow<List<HouseSaleArticle>>(listOf())
     val recentBuildingDetails = _recentBuildingDetails.asStateFlow()
 
-    private val _favoriteBuildingDetails = MutableStateFlow<List<HouseSaleArticle>>(listOf())
-    val favoriteBuildingDetails = _recentBuildingDetails.asStateFlow()
+    private val _wishList = MutableStateFlow<List<HouseSaleArticle>>(listOf())
+    val wishList = _wishList.asStateFlow()
 
     private val _errorState = MutableStateFlow<Exception?>(null)
     val errorState = _errorState.asStateFlow()
 
     init {
         loadRecentBuildingDetails()
-        loadFavoriteBuildingDetails()
+        loadWishList()
     }
 
-    private fun loadFavoriteBuildingDetails() {
-        _favoriteBuildingDetails.value = listOf()
+    private fun loadWishList() {
+        _wishList.value = listOf()
+        viewModelScope.launch {
+            getWishListUseCase().collect { result ->
+                result.onSuccess {
+                    _wishList.value = it
+                }.onFailure {
+                    _errorState.value = Exception(it.message)
+                }
+            }
+        }
     }
 
     private fun loadRecentBuildingDetails() {
