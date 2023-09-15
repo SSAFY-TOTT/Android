f7bc9661c5c6c69e8d5503bf154fee6c861aa853
ChoiUS <chldn323@gmail.com>
2023-09-15 22:36:20 +0900

    feat(#44): 최근 본 매물 보여주는 기능 추가

--- app/src/main/java/com/ssafy/tott/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/ssafy/tott/ui/home/HomeViewModel.kt
@@ -1,19 +1,27 @@
 package com.ssafy.tott.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ssafy.tott.domain.model.HouseSaleArticle
+import com.ssafy.tott.domain.usecase.GetRecentHouseArticlesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val getRecentHouseArticlesUseCase: GetRecentHouseArticlesUseCase
+) : ViewModel() {
     private val _recentBuildingDetails = MutableStateFlow<List<HouseSaleArticle>>(listOf())
     val recentBuildingDetails = _recentBuildingDetails.asStateFlow()
 
     private val _favoriteBuildingDetails = MutableStateFlow<List<HouseSaleArticle>>(listOf())
     val favoriteBuildingDetails = _recentBuildingDetails.asStateFlow()
+
+    private val _errorState = MutableStateFlow<Exception?>(null)
+    val errorState = _errorState.asStateFlow()
 
     init {
         loadRecentBuildingDetails()
@@ -25,9 +33,20 @@ class HomeViewModel @Inject constructor() : ViewModel() {
     }
 
     private fun loadRecentBuildingDetails() {
-        _recentBuildingDetails.value = listOf(
-            HouseSaleArticle(1, 5300, 10, 3, 123.0, 123.0, "강남구 역삼1동 멀티캠퍼스", 2003),
-            HouseSaleArticle(1, 9800, 28, 15, 123.5, 123.0, "중구 신당동", 2013),
-        )
+        // TODO 실제 조회한 매물 데이터로 수정 필요
+        val ids: List<Int>? = listOf(232, 6, 32, 8, 2326, 2651, 31, 10, 67)
+        viewModelScope.launch {
+            if (ids.isNullOrEmpty()) {
+                _recentBuildingDetails.value = emptyList()
+            } else {
+                getRecentHouseArticlesUseCase(ids).collect { result ->
+                    result.onSuccess {
+                        _recentBuildingDetails.value = it
+                    }.onFailure {
+                        _errorState.value = Exception(it.message)
+                    }
+                }
+            }
+        }
     }
 }
