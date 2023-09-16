695e6983644864ebf6f300deab8678407f1714bf
ChoiUS <chldn323@gmail.com>
2023-09-16 19:39:27 +0900

    feat(#51): 추가 자산 화면 구현

---
+++ app/src/main/java/com/ssafy/tott/ui/extramoney/ExtraMoneyActivity.kt
@@ -0,0 +1,12 @@
+package com.ssafy.tott.ui.extramoney
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssafy.tott.R
+
+class ExtraMoneyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_extra_money)
+    }
+}
