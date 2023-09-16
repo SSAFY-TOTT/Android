81fa8430e5bae29211a3fe9a56188ff637a36916
ChoiUS <chldn323@gmail.com>
2023-09-16 19:01:32 +0900

    fix(#50): 지역 정보를 가져오는 Service 수정

--- app/src/main/java/com/ssafy/tott/data/datasource/remote/service/RegionService.kt
+++ app/src/main/java/com/ssafy/tott/data/datasource/remote/service/RegionService.kt
@@ -1,18 +1,18 @@
 package com.ssafy.tott.data.datasource.remote.service
 
-import com.ssafy.tott.data.model.response.RegionPairResponse
+import com.ssafy.tott.data.model.response.DistrictListResponse
+import com.ssafy.tott.data.model.response.LegalDongListResponse
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface RegionService {
-    @POST("/api/region/auth/find/district")
-    suspend fun fetchGetDistrictList(): Response<Map<String, List<RegionPairResponse>>>
+    @GET("/api/region/auth/find/district")
+    suspend fun fetchGetDistrictList(): Response<DistrictListResponse>
 
     @GET("/api/region/auth/find/legal")
     suspend fun fetchGetLegalDongList(
         @Query("districtCode")
         code: Int
-    ): Response<Map<String, List<RegionPairResponse>>>
+    ): Response<LegalDongListResponse>
 }
