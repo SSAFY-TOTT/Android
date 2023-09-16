1a0b1c6add4fcba6b87f92eb51a0585113381363
ChoiUS <chldn323@gmail.com>
2023-09-16 11:36:10 +0900

    feat(#50): 지역 이름, 코드 Domain 클래스 구현

--- app/src/main/java/com/ssafy/tott/data/model/response/RegionPairResponse.kt
+++ app/src/main/java/com/ssafy/tott/data/model/response/RegionPairResponse.kt
@@ -1,10 +1,18 @@
 package com.ssafy.tott.data.model.response
 
 import com.google.gson.annotations.SerializedName
+import com.ssafy.tott.domain.model.RegionPair
 
 data class RegionPairResponse(
     @SerializedName("name")
     val name: String,
     @SerializedName("code")
-    val code: String,
-)
+    val code: Int,
+) {
+    companion object {
+        fun RegionPairResponse.toDomain() = RegionPair(
+            name = name,
+            code = code,
+        )
+    }
+}
