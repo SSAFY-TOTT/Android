a1ad7cfb5036805cf9acf12f84b44fbebc29f417
ChoiUS <chldn323@gmail.com>
2023-09-16 21:30:12 +0900

    feat(#51): 예산 저장 결과에 따라 UI 반영

--- app/src/main/java/com/ssafy/tott/ui/extramoney/ExtraMoneyActivity.kt
+++ app/src/main/java/com/ssafy/tott/ui/extramoney/ExtraMoneyActivity.kt
@@ -1,12 +1,91 @@
 package com.ssafy.tott.ui.extramoney
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.snackbar.Snackbar
 import com.ssafy.tott.R
+import com.ssafy.tott.databinding.ActivityExtraMoneyBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class ExtraMoneyActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityExtraMoneyBinding.inflate(layoutInflater) }
+    private val viewModel: ExtraMoneyViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_extra_money)
+        setContentView(binding.root)
+        initToolbar()
+        initStateObserve()
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(binding.toolbarExtraMoney)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search_map, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        android.R.id.home -> {
+            onBackPressed()
+            true
+        }
+
+        R.id.action_saveFilter -> {
+            saveExtraMoneyList()
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun saveExtraMoneyList() {
+        viewModel.saveExtraMoneyList(
+            binding.editTextDescription.text.toString() to
+                    binding.editTextMoneyExtraMoney.text.toString().toInt(),
+            binding.editTextDescription2.text.toString() to
+                    binding.editTextMoneyExtraMoney2.text.toString().toInt(),
+            binding.editTextDescription3.text.toString() to
+                    binding.editTextMoneyExtraMoney3.text.toString().toInt(),
+            binding.editTextDescription4.text.toString() to
+                    binding.editTextMoneyExtraMoney4.text.toString().toInt(),
+        )
+    }
+
+    private fun initStateObserve() {
+        lifecycleScope.launch {
+            viewModel.uiState.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED).collect {
+                when (it) {
+                    is ExtraMoneyViewModel.UiState.Loading -> {
+
+                    }
+
+                    is ExtraMoneyViewModel.UiState.Success -> {
+                        finish()
+                    }
+
+                    is ExtraMoneyViewModel.UiState.Error -> {
+                        Snackbar.make(
+                            binding.root, it.throwable.message ?: "오류 발생", Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+
+                    is ExtraMoneyViewModel.UiState.Wait -> {
+
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/ssafy/tott/ui/extramoney/ExtraMoneyViewModel.kt
+++ app/src/main/java/com/ssafy/tott/ui/extramoney/ExtraMoneyViewModel.kt
@@ -1,14 +1,29 @@
 package com.ssafy.tott.ui.extramoney
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ssafy.tott.domain.model.ExtraMoney
 import com.ssafy.tott.domain.usecase.SaveExtraMoneyUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class ExtraMoneyViewModel @Inject constructor(
     private val saveExtraMoneyUseCase: SaveExtraMoneyUseCase,
-) {
+) : ViewModel() {
+    private val _uiState = MutableStateFlow<UiState>(UiState.Wait)
+    val uiState = _uiState.asStateFlow()
+
+    sealed class UiState {
+        data object Wait : UiState()
+        data object Loading : UiState()
+        data class Error(val throwable: Throwable) : UiState()
+        data object Success : UiState()
+    }
+
     private val _extraMoneyList = MutableStateFlow<List<ExtraMoney>>(emptyList())
     val extraMoneyList = _extraMoneyList.asStateFlow()
 
@@ -16,6 +31,16 @@ class ExtraMoneyViewModel @Inject constructor(
         vararg pair: Pair<String, Int>
     ) {
         val list = pair.filter { it.first.isNotBlank() }.map { ExtraMoney(it.first, it.second) }
-        saveExtraMoneyUseCase(list)
+        if (list.isEmpty()) return
+        viewModelScope.launch {
+            _uiState.value = UiState.Loading
+            saveExtraMoneyUseCase(list).collect { result ->
+                result.onSuccess {
+                    _uiState.value = UiState.Success
+                }.onFailure {
+                    _uiState.value = UiState.Error(it)
+                }
+            }
+        }
     }
 }
